921276e975e6cfea0a5e310265f79f4c20f50d6d
Diksha wagh <waghdiksha935@gmail.com>
2025-10-20 01:53:42 +0530

    Update setup.py

--- maxheap/setup.py
+++ maxheap/setup.py
@@ -4,11 +4,11 @@ setup(
     name="pymaxheap",
     version="1.0.0",
     author="Diksha Wagh",
-    author_email="your_email@example.com",
+    author_email="waghdiksha935@gmail.com",
     description="A simple Python library for Max Heap operations built on heapq",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/Diksha-3905/pymaxheap",
+    url="https://github.com/Diksha-3905/Maxheap_for_myself",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
